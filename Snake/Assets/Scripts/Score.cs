4d6fb556055a002627ad38e504ae7d88959caeb5
Ruixin Li <kazezakimiyu@gmail.com>
2022-03-31 17:26:15 -0700

    adding the second level

--- Snake/Assets/Scripts/Score.cs
+++ Snake/Assets/Scripts/Score.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
@@ -17,5 +18,17 @@ public class Score : MonoBehaviour
     void Update()
     {
         score.text = "Score: " + scoreValue;
+    }
+
+    private void LevelUp()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            if (Score.scoreValue >= 2)
+            {
+                Debug.Log("Level Up");
+                SceneManager.LoadScene("Level2");
+            }
+        }
     }
 }
